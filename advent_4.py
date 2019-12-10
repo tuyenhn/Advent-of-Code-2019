04285042eaffd2d7c6474174b8e341316e6942cb
tuyenhn <hntuyen282@gmail.com>
2019-12-10 22:12:14 +0700

    first commit from pc

---
+++ advent_4.py
@@ -0,0 +1,18 @@
+import re
+
+cands = []
+for num in range(136818, 685979+1):
+    num = str(num)
+    flag = True
+    for i in range(len(num)-1):
+        if num[i] > num[i+1]:
+            flag = False
+
+    for uniq in set(str(num)):
+        if str(num).count(uniq) == 2:
+            if flag and len(set(num)) != 6:
+                cands.append(num)
+
+
+print(set(cands))
+print(len(set(cands)))
