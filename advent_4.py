360a3f8f0f8980ce8ddcb29783d8c98101ef65bb
tuyenhn <hntuyen282@gmail.com>
2019-12-10 22:44:22 +0700

    problem 4 is up to date

--- advent_4.py
+++ advent_4.py
@@ -1,5 +1,3 @@
-import re
-
 cands = []
 for num in range(136818, 685979+1):
     num = str(num)
@@ -8,11 +6,12 @@ for num in range(136818, 685979+1):
         if num[i] > num[i+1]:
             flag = False
 
+    # PART 2 - SOLVED
     for uniq in set(str(num)):
         if str(num).count(uniq) == 2:
             if flag and len(set(num)) != 6:
                 cands.append(num)
 
 
-print(set(cands))
+# print(set(cands))
 print(len(set(cands)))
