a30f961d5890e9be46f659a03250cee07c0edb69
tuyenhn <hntuyen282@gmail.com>
2019-12-14 11:50:22 +0700

    part 1 solved

--- advent_12.py
+++ advent_12.py
@@ -8,13 +8,13 @@ class planet:
         self.vel_z = 0
 
     def set_vel_x(self, vel_x):
-        self.vel_x = vel_x
+        self.vel_x += vel_x
 
     def set_vel_y(self, vel_y):
-        self.vel_y = vel_y
+        self.vel_y += vel_y
 
     def set_vel_z(self, vel_z):
-        self.vel_z = vel_z
+        self.vel_z += vel_z
 
     def update(self):
         self.x += self.vel_x
@@ -22,63 +22,59 @@ class planet:
         self.z += self.vel_z
 
 
-# p1 = planet([-2, 9, -5])
-# p2 = planet([16, 19, 9])
-# p3 = planet([0, 3, 6])
-# p4 = planet([11, 0, 11])
+p1 = planet([-2, 9, -5])
+p2 = planet([16, 19, 9])
+p3 = planet([0, 3, 6])
+p4 = planet([11, 0, 11])
 
-p1 = planet([-1, 0, 2])
-p2 = planet([2, -10, -7])
-p3 = planet([4, -8, 8])
-p4 = planet([3, 5, -1])
+# p1 = planet([-1, 0, 2])
+# p2 = planet([2, -10, -7])
+# p3 = planet([4, -8, 8])
+# p4 = planet([3, 5, -1])
 
 planets = [p1, p2, p3, p4]
 
-for _ in range(10):
+for _ in range(1000):
     x_pos = [p.x for p in planets]
-    x_pos.sort()
-    for p in planets:
-        idx = x_pos.index(p.x)
-        if idx == 0:
-            p.set_vel_x(3)
-        elif idx == 1:
-            p.set_vel_x(1)
-        elif idx == 2:
-            p.set_vel_x(-1)
-        else:
-            p.set_vel_x(-3)
-
     y_pos = [p.y for p in planets]
-    y_pos.sort()
-    for p in planets:
-        idx = y_pos.index(p.y)
-        if idx == 0:
-            p.set_vel_y(3)
-        elif idx == 1:
-            p.set_vel_y(1)
-        elif idx == 2:
-            p.set_vel_y(-1)
-        else:
-            p.set_vel_y(-3)
-
     z_pos = [p.z for p in planets]
-    z_pos.sort()
     for p in planets:
-        idx = z_pos.index(p.z)
-        if idx == 0:
-            p.set_vel_z(3)
-        elif idx == 1:
-            p.set_vel_z(1)
-        elif idx == 2:
-            p.set_vel_z(-1)
-        else:
-            p.set_vel_z(-3)
+        p_x = p.x
+        p_y = p.y
+        p_z = p.z
+        temp_vel_x = 0
+        temp_vel_y = 0
+        temp_vel_z = 0
+
+        for x_p in x_pos:
+            if p_x < x_p:
+                temp_vel_x += 1
+            elif p_x > x_p:
+                temp_vel_x -= 1
+        p.set_vel_x(temp_vel_x)
+
+        for y_p in y_pos:
+            if p_y < y_p:
+                temp_vel_y += 1
+            elif p_y > y_p:
+                temp_vel_y -= 1
+        p.set_vel_y(temp_vel_y)
+
+        for z_p in z_pos:
+            if p_z < z_p:
+                temp_vel_z += 1
+            elif p_z > z_p:
+                temp_vel_z -= 1
+        p.set_vel_z(temp_vel_z)
 
     for p in planets:
         p.update()
 
+
+# PART ONE - SOLVED
 total_es = []
 for p in planets:
-    total_es.append((p.x + p.y + p.z) * (p.vel_x + p.vel_y + p.vel_z))
+    total_es.append((abs(p.x) + abs(p.y) + abs(p.z)) *
+                    (abs(p.vel_x) + abs(p.vel_y) + abs(p.vel_z)))
 
 print(sum(total_es))
